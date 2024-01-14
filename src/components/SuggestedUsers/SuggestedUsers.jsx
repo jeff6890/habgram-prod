f6a2a704c62aa82dfe6ce8b03930b6b6d510cce6
jeff6890 <jeffyuv@gmail.com>
2024-01-13 20:57:27 -0500

    Update

--- src/components/SuggestedUsers/SuggestedUsers.jsx
+++ src/components/SuggestedUsers/SuggestedUsers.jsx
@@ -30,8 +30,8 @@ const SuggestedUsers = () => {
 
 			<Box fontSize={12} color={"gray.500"} mt={5} alignSelf={"start"}>
 				© 2024 Habgram{" "}
-				{/* <Link href='https://www.youtube.com/@asaprogrammer_' target='_blank' color='blue.500' fontSize={14}>
-					As a Programmer
+				{/* <Link href='' target='_blank' color='blue.500' fontSize={14}>
+					
 				</Link> */}
 			</Box>
 		</VStack>
